e015b34d4536a62702111bfc728dc1ba7a464375
Sogiefa-12 <131720010+Sogiefa-12@users.noreply.github.com>
2024-12-20 04:03:14 +0100

    commit

--- my-App/rollup.config.js
+++ my-App/rollup.config.js
@@ -2,7 +2,7 @@ import { rollup } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/main.js',
+  input: './src/main.js',
   output: {
     file: 'public/bundle.js',
     format: 'es',
